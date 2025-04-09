92abdd612196dcccdc03bc3b83f384290e40c5ce
Swathi <swathimn.parvam1@gmail.com>
2025-04-09 15:58:17 +0530

    initial  commmit

---
+++ Eclipse workspace.java/springSecurityDemo2/src/main/java/com/springSecurityDemo/springSecurityDemo/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.springSecurityDemo.springSecurityDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping({"","/"})
+	public String home() {
+		return "home";
+	}
+}
